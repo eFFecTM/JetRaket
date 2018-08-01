ea364ae8ef59622542e56801476e68b9b87aef9c
Jan De Laet <jan.delaet@hotmail.com>
2018-08-01 20:47:37 +0200

    movement WIP

---
+++ core/src/com/mygdx/game/Screen/GameScreen.java
@@ -0,0 +1,113 @@
+package com.mygdx.game.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.JetRaket;
+import com.mygdx.game.sprites.Rocket;
+
+public class GameScreen implements Screen {
+
+    private final JetRaket game;
+    private OrthographicCamera cam;
+    private Stage stage;
+
+    private Rocket rocket;
+    private Texture bg;
+    private Skin touchpadSkin;
+    private Touchpad touchpad;
+
+
+    public GameScreen(final JetRaket game) {
+        this.game = game;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, game.WIDTH, game.HEIGHT);
+
+        rocket = new Rocket(40,200);
+        bg = new Texture("background.jpg");
+
+        touchpadSkin = new Skin();
+		//Set background image
+		touchpadSkin.add("touchBackground", new Texture("touchBackground.png"));
+		//Set knob image
+		touchpadSkin.add("touchKnob", new Texture("touchKnob.png"));
+		//Create TouchPad Style
+        Touchpad.TouchpadStyle touchpadStyle = new Touchpad.TouchpadStyle();
+		//Create Drawable's from TouchPad skin
+		Drawable touchBackground = touchpadSkin.getDrawable("touchBackground");
+		Drawable touchKnob = touchpadSkin.getDrawable("touchKnob");
+		//Apply the Drawables to the TouchPad Style
+		touchpadStyle.background = touchBackground;
+		touchpadStyle.knob = touchKnob;
+		//Create new TouchPad with the created style
+		touchpad = new Touchpad(10, touchpadStyle);
+		//setBounds(x,y,width,height)
+		touchpad.setBounds(15, 15, 200, 200);
+
+		stage = new Stage(new ScreenViewport());
+		stage.addActor(touchpad);
+		Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        cam.update();
+        game.batch.setProjectionMatrix(cam.combined);
+        game.batch.begin();
+        game.batch.draw(bg, 0, 0);
+        game.batch.end();
+        stage.draw();
+        handleInput();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        rocket.dispose();
+        bg.dispose();
+        touchpadSkin.dispose();
+        stage.dispose();
+    }
+
+    private void handleInput() {
+//        rocket.move(JetRaket.touchpad.getKnobPercentX()*10000,JetRaket.touchpad.getKnobPercentY()*10000);
+//        System.out.println(JetRaket.touchpad.getKnobPercentX() + " - " + JetRaket.touchpad.getKnobPercentY());
+
+        if(Gdx.input.justTouched()) {
+            game.setScreen(new MenuScreen(game));
+            dispose();
+        }
+    }
+}
---
+++ core/src/com/mygdx/game/Screen/MenuScreen.java
@@ -0,0 +1,73 @@
+package com.mygdx.game.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.JetRaket;
+
+public class MenuScreen implements Screen {
+
+    private final JetRaket game;
+    private OrthographicCamera cam;
+
+    private Texture playBtn;
+
+    public MenuScreen(final JetRaket game) {
+        this.game = game;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, game.WIDTH, game.HEIGHT);
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        cam.update();
+        game.batch.setProjectionMatrix(cam.combined);
+        game.batch.begin();
+        game.batch.draw(playBtn,cam.position.x - playBtn.getWidth()/2, cam.position.y);
+        game.font.draw(game.batch,"JetRaket",-cam.position.x ,cam.position.y+100,game.WIDTH, Align.center, false);
+        game.batch.end();
+        handleInput();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        playBtn.dispose();
+    }
+
+    private void handleInput() {
+        if(Gdx.input.justTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+}
